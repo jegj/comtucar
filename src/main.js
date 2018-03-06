7d4cf622369f6d841801c83afa54008860d69f4b
Javier Galarza <jegj57@gmail.com>
2018-03-05 22:49:05 -0500

    Add util features

---
+++ src/main.js
@@ -0,0 +1,39 @@
+// Navigation Close on Click
+//-------------------------------------------------------------------------------
+// $(document).ready(function () {
+// 	$(".navbar-nav li a").on('click', function (event) {
+// 		// $(".navbar-collapse").collapse('hide');
+// 	});
+// });
+
+// Scroll To Animation
+//-------------------------------------------------------------------------------
+$(document).ready(function () {
+	// $('body').scrollspy({ target: '#navigation-top', offset: 100 });
+
+	var scrollTo = $(".scroll-to");
+	
+	scrollTo.on('click', function (event) {
+		//HIDE MODAL
+		// $('.modal').modal('hide');
+		var position = $(document).scrollTop();
+		var scrollOffset = 99;
+	
+		var marker = $(this).attr('href');
+		$('html, body').animate({ scrollTop: $(marker).offset().top - scrollOffset }, 'slow');
+		return false;
+	});
+});
+
+
+// Scroll Up Btn
+//-------------------------------------------------------------------------------
+$(window).scroll(function () {
+	if ($(this).scrollTop() > 100) {
+		$('.scroll-up-btn').removeClass("animated fadeOutRight");
+		$('.scroll-up-btn').fadeIn().addClass("animated fadeInRight");
+	} else {
+		$('.scroll-up-btn').removeClass("animated fadeInRight");
+		$('.scroll-up-btn').fadeOut().addClass("animated fadeOutRight");
+	}
+});
