0682ae0216c33c1ab03a75964516f8becc0ccf15
Javier <jegj57@gmail.com>
2018-03-17 18:27:13 -0500

    Add more fields on reservation form

--- src/main.js
+++ src/main.js
@@ -59,9 +59,37 @@ $(window).scroll(function () {
 	}
 });
 
+
+$.fn.datepicker.dates['es'] = {
+	days: ["Domingo", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado"],
+	daysShort: ["Dom", "Lun", "Mar", "Mie", "Jue", "Vie", "Sab"],
+	daysMin: ["Do", "Lu", "Ma", "Mi", "Ju", "Vi", "Sa"],
+	months: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"],
+	monthsShort: ["Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dic"],
+	today: "Hoy",
+	clear: "Limpiar",
+	format: "mm/dd/yyyy",
+	titleFormat: "MM yyyy", /* Leverages same syntax as 'format' */
+	weekStart: 0
+};
+
+//DATEPICKER
+$('.datepicker').datepicker({
+	todayHighlight: true,
+	startDate: new Date(),
+	language: 'es'
+}).on('changeDate', function () {
+	$(this).datepicker('hide');
+});
+
+
+// PHONE ACTION
+
 $('#callbtn').click(function(e){
 	window.location.href = 'tel:+51982880414';
 });
+
+//MODAL
 
 $('.show-inquiry-modal').on('click', function () {
 	// var object = $(this).data('object');
