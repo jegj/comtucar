2daafa9c858fe6bb1f983156f60fe0baf5cacb69
Javier Galarza <jegj57@gmail.com>
2018-04-19 09:49:05 -0500

    Add TRANS to book form

--- src/main.js
+++ src/main.js
@@ -273,6 +273,172 @@ $(document).ready(function () {
 		'services:communalareas:lookput': {
 			es: 'Mirador con vistas para tomar fotografias a los nevados y al paisaje andino',
 			en: 'Tourist lookout to take snapshots of the snow-capped mountains'
+		},
+		'touristic': {
+			es: 'Lugares Turísticos',
+			en: 'Touristic Places'
+		},
+		'touristic:des': {
+			es: 'Carhuaz bajo el tutelar del Majestuoso Nevado Hualcán cuenta con un potencial turistico que sorprende a propios y extraños.',
+			en: 'Carhuaz is located at the foot of  the Majestic Hualcan snow capped mountain and has a great deal of touristic spots nearby.'
+		},
+		'touristic:peak': {
+			es: 'Punta Olímpica',
+			en: 'Punta Olimpica (Olympic Peak)'
+		},
+		'touristic:peak:des': {
+			es: 'A 4,890 m.s.n.m. ubicado en la carretera Carhuaz - Shilla-Chacas, donde se puede observar y palpar la nieve perpetua y las aguas cristalinas de vistosas lagunas, asi como practicar el "Siqui Esqui" o sea deslizarse sentado sobre la nieve. Es el lugar del tunel vehicular mas largo del Peru y el de mayor altitud en el mundo.',
+			en: 'At 4,890 meters above sea level, located in the Carhuaz – Chacas road, you will be able to see and touch the snow as well as admire beautiful lakes.  If you dare you can practice the ‘siki ski’ or ‘butt slide’.  It is the largest vehicular tunnel in Perú  and the highest in the world.'
+		},
+		'touristic:chancos': {
+			es: 'Baños Termales de Chancos',
+			en: 'Chancos Thermal Spa'
+		},
+		'touristic:chancos:des': {
+			es: 'Sea por turismo o por razones de salud, es el lugar ideal por las propiedades curativas de este lugar. Sus aguas varian de 68 a 72^C. Existen cuevas, donde se pueden ingresar hojas de eucalipto y por la accion del vapor emanan un aroma que penetra a los pulmones favoreciendo a quienes padecen de problemas musculares y respiratorios.',
+			en: 'If you travel for pleasure or for medical reasons, this natural spa has healing properties.  The water and cave temperatures range from 68~ to 72~C.  You can bring your own eucalyptus leaves to the caves and the vapours  released help relieve muscle and respiratory problems.'
+		},
+		'touristic:honcopampa': {
+			es: 'Centro Arqueologico Honcopampa',
+			en: 'Honcopampa Archeologial Site'
+		},
+		'touristic:honcopampa:des': {
+			es: 'Ha sido el escenario de los grupos etnicos 700 a 1000 D.C. Son doce hectareas divididos en dos sectores: Residencial y ceremonial de tumbas.  Las viviendas son rectangulares con patio central y las tumbas son necropolis de chullpas o mausoleos de dos pisos.',
+			en: 'A civilization flourished on this site between 700 and 100 AC.   Part of the residences and ceremonial tombs can still be appreciated despite time passing.'
+		},
+		'touristic:huascaran': {
+			es: 'Nevado Huascaran',
+			en: 'Huascaran Mountain'
+		},
+		'touristic:huascaran:des': {
+			es: 'Es el nevado mas alto del Perú a 6768 m.s.n.m. y el más popular.  Se ubica en Yungay, a 20 kms de Carhuaz.  Al pie se encuentran las lagunas de Llanganuco, de aguas turquezas y los bosques de quenuales.  Es una zona impresionante y su caracterisitca principal es la biodiversividad de flora y fauna.',
+			en: 'It is the highest peak in Perú at 6768 metres above sea level and the most popular.  It is located in Yungay, 20 kms north of Carhuaz.  The famous Llanganuco lakes are located nearby with its turquoise waters that change colour throughout the day.  It is an ideal location to appreciate the biodiversity of the local flora and fauna.'
+		},
+
+
+		'booking:label': {
+			es: 'Reserva',
+			en: 'Booking'
+		},
+		'booking:select': {
+			es: 'Seleccione su Habitación',
+			en: 'Select your room'
+		},
+		'booking:single':{
+			es: 'Simple (S/.60)',
+			en: 'Single (S/.60)'
+		},
+		'booking:single:easter':{
+			es: 'Simple - Semana Santa o Fiestas Patrias(S/.100)',
+			en: 'Single – Easter or last week of July S/.100'
+		},
+		'booking:double':{
+			es: 'Doble/Matrimonial(S/.100)',
+			en: 'Double/Matrimonial S/.100'
+		},
+		'booking:double:easter':{
+			es: 'Doble/Matrimonial - Semana Santa o Fiestas Patrias(S/.160)',
+			en: 'Double/Matrimonial – Easter of last week of July S/.160'
+		},
+		'booking:triple':{
+			es: 'Triple(S/.140)',
+			en: 'Triple(S/.140)'
+		},
+		'booking:triple:easter':{
+			es: 'Triples - Semana Santa o Fiestas Patrias(S/.180)',
+			en: 'Triple – Easter or last week of July S/.180'
+		},
+		'booking:matrimonial':{
+			es: 'Suite Matriomonial(S/.160)',
+			en: 'VIP Suite  S/.160'
+		},
+		'booking:matrimonial:easter':{
+			es: 'Suite Matriomonial - Semana Santa o Fiestas Patrias(S/.230)',
+			en: 'VIP Suite – Easter or last week of July S/.230'
+		},
+		'booking:passenger':{
+			es: 'Total del Pasajeros',
+			en: 'Total number of passengers'
+		},
+		'booking:beds':{
+			es: 'Cama Adicionales',
+			en: 'Additional beds required'
+		},
+		'booking:breakfast':{
+			es: '(*)Los desayunos son incluidos en la tarifa de Temporada Alta',
+			en: '(*) Breakfast is included in high season rates'
+		},
+		'booking:cbreakfast':{
+			es: 'Desayuno Continental',
+			en: 'Continental Breakfast'
+		},
+		'booking:abreakfast':{
+			es: 'Desayuno Americano',
+			en: 'American Breakfast'
+		},
+		'booking:arrival':{
+			es: 'Fecha de Arrivo',
+			en: 'Arrival Date'
+		},
+		'booking:harrival':{
+			es: 'Hora',
+			en: 'Estimated time of arrival'
+		},
+		'booking:harrival:label':{
+			es: 'Seleccione la Hora',
+			en: 'Select the Hour'
+		},
+		'booking:depature':{
+			es: 'Fecha de Salida',
+			en: 'Departure Date'
+		},
+		'booking:hdepature':{
+			es: 'Hora',
+			en: 'Estimated time of departure'
+		},
+		'booking:personallabel': {
+			es: 'Información Personal',
+			en: 'Personal Information'
+		},
+		'booking:name': {
+			es: 'Nombre',
+			en: 'Name'
+		},
+		'booking:surname': {
+			es: 'Surname',
+			en: 'Apellido'
+		},
+		'booking:address': {
+			es: 'Dirección',
+			en: 'Address'
+		},
+		'booking:city': {
+			es: 'Ciudad',
+			en: 'City'
+		},
+		'booking:phonenumber': {
+			es: 'Teléfono',
+			en: 'Telephone'
+		},
+		'booking:email': {
+			es: 'Correo',
+			en: 'Email Address'
+		},
+		'booking:addtional': {
+			es: 'Información Adicional',
+			en: 'Additional Information'
+		},
+		'booking:aware': {
+			es: 'Tomar en cuenta que este formulario no reserva totalmente la habitacion. Solo solicita una reserva para dicha habitacion. Los precios estan sujetos cambios de acuerdo a la temporada, asi como cualquier otro detalle que especifique.',
+			en: '!Please be aware that this form does not automatically reserve a room,  it is an enquiry to reserve.'
+		},
+		'booking:aware:black':{
+			es: 'Nos pondremos en contacto brevemente via email o telefonica. Gracias!',
+			en: 'We will get in touch by phone or email to confirm.  Thank you.'
+		},
+		'booking:button': {
+			es: 'Verificar Disponibilidad',
+			en: 'Check Availability'
 		}
 	};
 	var _t = $('body').translate({ lang: "es", t: t });
